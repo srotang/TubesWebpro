b4164f5d6cec20915cc2061f6718d02aa0c27309
kikisuriki <admiralrizki25@gmail.com>
2019-04-12 20:55:26 +0700

    reeeeeeeeeeeeeeeeeee

---
+++ application/models/M_foto.php
@@ -0,0 +1,32 @@
+<?php
+ 
+class M_foto extends CI_model
+{
+
+	public function getAllfoto()
+	{
+		//use query builder to get data table "foto"
+		return $this->db->get('foto')->result_array();
+	}
+
+	public function tambahDatafoto()
+	{
+		$data = [
+			"namaF" => $this->input->post('namaF', true)
+		];
+
+		//use query builder to insert $data to table "foto"
+		$this->db->insert('foto', $data);
+	}
+
+	public function hapusDatafoto()
+	{
+		$this->db->delete('foto');
+	}
+
+	public function getfotoBynama($namaF)
+	{
+		 
+		return $this->db->get_where('foto', ['namaf'=>$namaF])->row_array();
+	}
+}
