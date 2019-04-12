c7a629195a0f379c2b30a14f9f43c1fb810574b6
kikisuriki <admiralrizki25@gmail.com>
2019-04-12 20:38:56 +0700

    tambahan

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/CI-Deviantart/';
+$config['base_url'] = 'http://localhost/TubesWebpro/';
 
 /*
 |--------------------------------------------------------------------------
