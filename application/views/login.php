56e1b94cfa90a447d4c02a91bfb9a7fa72479797
Sri Tangkelangi <sritangkelangi@gmail.com>
2019-04-18 00:50:03 +0700

    login terfix

--- application/views/login.php
+++ application/views/login.php
@@ -1,15 +1,14 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!--Offline bootstrap-->
-    <script type="text/javascript" src="bootstrap/js/jquery-3.3.1.min.js"></script>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Login</title>
+    <title>Log In | DeviantArt</title>
 
     <style>
     body{
@@ -56,41 +55,46 @@
       color: white;
     }
     </style>
-    </head>
-<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
-<body>
+  </head>
+  <body>
     <form class="form-login" action="">
-      <table>
-        <tbody style="position: absolute; left: 70px">
-          <tr>
-            <td class="table-label">
-                <label>Username or Email</label>
-                <input class="text" type="text" id="login_username" name="username" value="" autofocus="autofocus"style="width:220px;">
-            </td>
+        <table>
+          <tbody style="position: absolute; left: 70px">
+            <tr>
+              <td class="table-label">
+                  <label>Username or Email</label>
+                  <input class="text" type="text" id="login_username" name="username" value="" autofocus="autofocus"style="width:220px;">
+              </td>
+            </tr>
+            <tr>
+              <td class="table-label">
+                  <label>Password</label>
+                  <input class="text" type="password" id="login_password" name="password" style="width:220px;">
+              </td>
+            </tr>
+            <tr>
+              <td>
+                  <input type="checkbox" id="remember_me" checked="checked" name="remember_me"> Stay logged in
+              </td>
           </tr>
           <tr>
-            <td class="table-label">
-                <label>Password</label>
-                <input class="text" type="password" id="login_password" name="password" style="width:220px;">
-            </td>
+              <td class="table-sign">                
+                <input type="submit" value="Log In">
+              </td>
           </tr>
           <tr>
-             <td>
-                <input type="checkbox" id="remember_me" checked="checked" name="remember_me"> Stay logged in
-             </td>
-         </tr>
-         <tr>
-            <td class="table-sign">                
-              <input type="submit" value="Log In">
-            </td>
-         </tr>
-         <tr>
-            <td>
-              <a href="#">Forgot Password or Username?</a>
-            </td>
-         </tr>
-        </tbody>
-      </table>
-    </form>
-</body>
+              <td>
+                <a href="#">Forgot Password or Username?</a>
+              </td>
+          </tr>
+          </tbody>
+        </table>
+      </form>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
