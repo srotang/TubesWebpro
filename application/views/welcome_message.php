fa5908dfa741f76e6f28cafd9bddb07e7e174b3c
Sri Tangkelangi <sritangkelangi@gmail.com>
2019-04-20 16:36:59 +0700

    srotang comeback

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,7 +5,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
-<!-- halo ayamQ, lagi makan apa? ayam -->
 	<style type="text/css">
 	
 	::selection { background-color: #E13300; color: white; }
