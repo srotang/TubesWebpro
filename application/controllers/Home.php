afd11402aaa1b62711ee1e69f7f5022baa3c77b2
agungfazrulhaq <47171085+agungfazrulhaq@users.noreply.github.com>
2019-04-23 09:41:08 +0700

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,6 +3,7 @@
 class Home extends CI_Controller{
 	public function index(){
 		$this->load->view('template/default-header');
+		$this->load->view('template/navigator');
 		$this->load->view('home/browse');
 		$this->load->view('template/footer');
 	}
