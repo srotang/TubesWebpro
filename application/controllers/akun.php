1630446750d0b9da84efb761075b142b90bd0751
agungfhaq <agungfazrulhaq95@gmail.com>
2019-04-23 21:49:32 +0700

    ---

---
+++ application/controllers/akun.php
@@ -0,0 +1,90 @@
+<?php
+ 
+class Mahasiswa extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		
+		$this->load->model('M_akun');
+		
+		$this->load->library('form_validation');
+	}
+
+
+	public function index()
+	{
+
+		$data['judul'] = 'Daftar Mahasiswa';
+		$data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
+		if ($this->input->post('keyword')) {
+			$data['mahasiswa'] = $this->Mahasiswa_model->cariDataMahasiswa();
+		}
+		$this->load->view('templates/header', $data);
+		$this->load->view('mahasiswa/index', $data);
+		$this->load->view('templates/footer');
+	}
+
+	public function tambah()
+	{
+		$data['judul'] = 'Form Tambah Data Mahasiswa';
+
+		//from library form_validation, set rules for nama, nim, email = required
+		$this->form_validation->set_rules('nama', 'Nama', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'required');
+		//conditon in form_validation, if user input form = false, then load page "tambah" again
+		if($this->form_validation->run() == FALSE){
+			$this->load->view('',$data);
+			$this->load->view('',$data);
+			$this->load->view('');
+		}
+		//else, when successed {
+		else {
+		//call method "tambahDataMahasiswa" in Mahasiswa_model
+		$this->M_akun->tambahDataakun();
+		//use flashdata to to show alert "added success"
+		$this->session->set_flashdata('flash', 'ditambahkan');
+		//back to controller mahasiswa }
+		redirect('main');
+		}
+	}
+
+	public function hapus($id)
+	{
+		//call method hapusDataMahasiswa with parameter id from mahasiswa_model
+		$this->Mahasiswa_model->hapusDataMahasiswa($id);
+		//use flashdata to show alert "dihapus"
+		$this->session->set_flashdata('flash', 'dihapus');
+		//back to controller mahasiswa
+		redirect('mahasiswa');
+	}
+
+	public function ubah($id)
+	{
+		$data['judul'] = 'Form Ubah Data Mahasiswa';
+
+		$data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
+		$data['jurusan'] = ['Teknik Informatika', 'Teknik Industri', 'Teknik Elektro', 'DKV', 'MBTI'];
+
+		//from library form_validation, set rules for nama, nim, email = required
+		$this->form_validation->set_rules('nama', 'Nama', 'required');
+		$this->form_validation->set_rules('nim', 'NIM', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'required');
+		//conditon in form_validation, if user input form = false, then load page "ubah" again
+		if($this->form_validation->run() == FALSE){
+			$this->load->view('templates/header',$data);
+			$this->load->view('mahasiswa/ubah',$data);
+			$this->load->view('templates/footer');
+		}
+		//else, when successed {
+		else {
+		//call method "ubahDataMahasiswa" in Mahasiswa_model
+		$this->Mahasiswa_model->ubahDataMahasiswa();
+		//use flashdata to to show alert "data changed successfully"
+		$this->session->set_flashdata('flash', 'data changed successfully');
+		//back to controller mahasiswa }
+		redirect('mahasiswa');
+		}
+	}
+}
---
+++ application/views/footer.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--Offline bootstrap-->
+    <script type="text/javascript" src="bootstrap/js/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+
+    <title>Footer</title>
+
+    <style>
+    .footer-page{
+        background-color :#4E5F55;
+        
+                <div class="col-md-1">
+    }
+    .col p{
+        font-size: 11px;
+    }
+    .col ul li{
+        display: inline;
+        margin: 5px;
+        font-size: 11px;
+        color: white;
+        height: 50px;
+    }
+    </style>
+</head>
+<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+<body>
+<footer class="footer-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col">
+                    <ul>
+                        <li>©2019 DeviantArt. All rights reserved</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                        <li>Help & FAQ</li>
+                        <li>Privacy Policy</li>
+                        <li>Copyright Policy</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
