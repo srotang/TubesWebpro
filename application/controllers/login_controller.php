e803eeec9807618e5f8c17d84ec5492289fac0e7
Sri Tangkelangi <sritangkelangi@gmail.com>
2019-04-23 18:47:27 +0700

    gajadi

--- application/controllers/login_controller.php
+++ application/controllers/login_controller.php
@@ -1,16 +1,11 @@
 <?php
- 
-class Login extends CI_Controller
-{
 
-	public function __construct()
-	{
-		parent::__construct();
-		
+class Login extends CI_Controller{
+	public function index(){
+		$this->load->view('template/default-header');
+		$this->load->view('template/navigator');
+		$this->load->view('home/browse');
+		$this->load->view('template/footer');
 	}
-// 
-	public function index()
-	{
-		$this->load->view('login');
-    }
+
 }
