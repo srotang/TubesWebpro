b8aabb5feb7582a6a96533858af7035d01c9da92
kikisuriki <admiralrizki25@gmail.com>
2019-04-23 22:47:27 +0700

    ssss

--- application/views/home/login.php
+++ application/views/home/login.php
@@ -1,31 +1,23 @@
 <!doctype html>
 <html lang="en">
   <head>
-<<<<<<< HEAD
     
     <title>Log In | DeviantArt</title>
-=======
->>>>>>> b58a4b5dc519e9664c424be858db9257f6a1c423
 
     <style>
-  .container-form-login{
-    width:100%;
-  }
+    .container-form-login{
+      width:100%;
+    }
     body{
         background-color : #CDD8C8;
     }
     .form-login{
         background-color : #D8E4D6;
         border: 1px solid #D8E4D6;
-        position: absolute;
-        top: 100px;
+        margin:auto;
+        margin-top:45px;
         height : 180px;
         width : 460px;
-<<<<<<< HEAD
-=======
-        margin : 20px 359px 20px 379px;
-        margin:auto;
->>>>>>> b58a4b5dc519e9664c424be858db9257f6a1c423
         padding : 20px 20px 20px 20px;
         font-size: 12px;
         transform : none;
@@ -80,14 +72,10 @@
   </head>
   <body>
     <?= $this->session->flashdata("sukses")?>
-<<<<<<< HEAD
     <center>
-=======
-    <div class="container-form-login">
->>>>>>> b58a4b5dc519e9664c424be858db9257f6a1c423
     <form class="form-login" action="<?= base_url('index.php/Login/asup') ?>" method="post">
         <table class="tale-form-login">
-          <tbody style="position: absolute; left: 70px">
+          <tbody style="">
             <tr>
               <td class="table-label">
                   <label>Username or Email</label>
@@ -118,12 +106,7 @@
           </tbody>
         </table>
       </form>
-<<<<<<< HEAD
   </center>
-=======
-      </div>
-    <tr class="footer-page" <?= base_url('template/footer')?>></tr>
->>>>>>> b58a4b5dc519e9664c424be858db9257f6a1c423
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 </body>
